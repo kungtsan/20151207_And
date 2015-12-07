dccc551e94d22ba82e3eaa1e4d0011f1759c9f41
kungtsan <kungtsan@gmail.com>
2015-12-07 21:10:13 +0800

    20151207-1

---
+++ SimpleUI/app/src/main/java/com/example/simpleui/DrinkMenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.simpleui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class DrinkMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_menu);
+    }
+
+    public void add(View view) {
+        Button button = (Button) view;
+        int number = Integer.parseInt(button.getText().toString());
+        number++;
+        button.setText(String.valueOf(number));
+    }
+}
